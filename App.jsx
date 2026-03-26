bdd617bd8b37a7458ca6edceacbdb9f874e2d9a4
Pedro Acosta <163659584+pedroam2024@users.noreply.github.com>
2026-03-26 10:09:14 -0300

    s

---
+++ App.jsx
@@ -0,0 +1,5 @@
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+  <rect width="32" height="32" fill="#131313"/>
+  <rect x="0" y="0" width="32" height="2" fill="#b8945a"/>
+  <text x="16" y="22" font-family="Georgia,serif" font-size="14" font-weight="700" fill="#b8945a" text-anchor="middle">PA</text>
+</svg>
